de08607bf7007108f07379745a46917594a6a18f
Daniel Mendonça <asmendoncadaniel@gmail.com>
2016-10-21 04:38:56 +0100

    correction to costumers json response

--- acme_remote/api/controllers/costumers.js
+++ acme_remote/api/controllers/costumers.js
@@ -7,10 +7,12 @@ module.exports = {
     getCostumers : getCostumers
 };
 
+//sql costumer insertion INSERT INTO public.costumer VALUES ('daniel', 'dm', 'pass', '1234', '123456789', current_date, 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', now(), now());
+
 function getCostumers(req, res){
     Costumer.findAll({
         attributes: ["username","uuid"]
     }).then(function(costumers){
-        res.json(JSON.stringify(costumers));
+        res.json(costumers);
     });
 }
