28ebed87706052e609f195b8b1d6a58343befc31
Daniel Mendonça <asmendoncadaniel@gmail.com>
2016-11-05 14:13:31 +0000

    View a request details feature

---
+++ ClientApp/app/src/main/java/pt/up/fe/cmov16/client/clientapp/ui/RequestDetailActivity.java
@@ -0,0 +1,13 @@
+package pt.up.fe.cmov16.client.clientapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_detail);
+    }
+}
