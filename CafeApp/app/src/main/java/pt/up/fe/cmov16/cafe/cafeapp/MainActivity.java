49bb3fb0c91cdbcf4ef9872770c609cc30bf43d0
Miguel Nunes <ei12032@fe.up.pt>
2016-11-07 11:41:32 +0000

    saving products in local DB

--- CafeApp/app/src/main/java/pt/up/fe/cmov16/cafe/cafeapp/MainActivity.java
+++ CafeApp/app/src/main/java/pt/up/fe/cmov16/cafe/cafeapp/MainActivity.java
@@ -3,10 +3,22 @@ package pt.up.fe.cmov16.cafe.cafeapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import io.swagger.client.ApiInvoker;
 import io.swagger.client.api.DefaultApi;
+import io.swagger.client.model.Products;
+import pt.up.fe.cmov16.cafe.cafeapp.database.ProductContract;
 import pt.up.fe.cmov16.cafe.cafeapp.ui.ProcessRequestActivity;
 import pt.up.fe.cmov16.cafe.cafeapp.ui.ScanQRCodeActivity;
 
@@ -22,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ApiInvoker.initializeInstance();
-
+        loadProducts();
         findViewById(R.id.scanQRCode).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,5 +56,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         }
+    }
+
+    private void loadProducts() {
+        DefaultApi api = new DefaultApi();
+        //if updatedAt was saved before, the skip this step and use its value in lastDate
+        String lastUpdated = ProductContract.lastUpdatedProductDate(this);
+        boolean stored = !lastUpdated.isEmpty();
+        if (!stored)
+            lastUpdated = getDefaultTimestamp();
+
+        Log.e("products", lastUpdated);
+        api.getProducts(lastUpdated,
+                new Response.Listener<Products>() {
+                    @Override
+                    public void onResponse(Products response) {
+                        if (response.getProducts().size() > 0) {
+                            ProductContract.updateProducts(MainActivity.this, response.getProducts());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,
+                                "Connection failed, using local products", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private String getDefaultTimestamp() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        java.util.Date date;
+        Timestamp ts;
+        try {
+            date = dateFormat.parse("1900/01/01");
+            long time = date.getTime();
+            ts = new Timestamp(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            ts = new Timestamp(0);
+        }
+
+
+        return ts.toString().replace(' ', 'T');
     }
 }
---
+++ CafeApp/app/src/main/java/pt/up/fe/cmov16/cafe/cafeapp/database/ProductContract.java
@@ -0,0 +1,210 @@
+package pt.up.fe.cmov16.cafe.cafeapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.swagger.client.model.Product;
+import pt.up.fe.cmov16.cafe.cafeapp.R;
+
+/**
+ * Product model and related db operations
+ */
+
+public final class ProductContract {
+
+    private static final String TAG = ProductContract.class.toString();
+    private static final String SELECT_ALL_ACTIVE_PRODUCTS =
+            "SELECT * FROM " + ProductEntry.TABLE_NAME
+                    + " where " + ProductEntry.COLUMN_NAME_ACTIVE + " = 1"
+                    + " order by " + ProductEntry.COLUMN_NAME_NAME + " asc";
+
+    public ProductContract() {
+    }
+
+    /**
+     * Update local database products
+     *
+     * @param context
+     * @param products
+     * @return products that are active
+     */
+    public static void updateProducts(Context context, List<Product> products) {
+        if (context == null) {
+            Log.e(TAG, "NULL CONTEXT UPDATING PRODUCTS DB");
+            return;
+        }
+        String updatedAt = "";
+        if (products == null || products.isEmpty()) {
+            return;
+        }
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        if (db == null) {
+            Log.e(TAG, "NULL DB UPDATING PRODUCTS DB");
+            return;
+        }
+        int newProducts = 0, updatedProducts = 0;
+        for (Product product : products) {
+            if (product == null) {
+                Log.e(TAG, "products array has null product");
+                continue;
+            }
+
+            if (updatedAt.isEmpty())
+                updatedAt = product.getUpdatedAt();
+            else if (product.getUpdatedAt().compareTo(updatedAt) > 0)
+                updatedAt = product.getUpdatedAt();
+
+            Cursor c = db.rawQuery(selectByProductIDQuery(product.getId()), null);
+            if (c == null)
+                continue;
+
+            ContentValues values = new ContentValues();
+            values.put(ProductEntry.COLUMN_NAME_ID, product.getId());
+            values.put(ProductEntry.COLUMN_NAME_NAME, product.getName());
+            values.put(ProductEntry.COLUMN_NAME_ACTIVE, product.getActive());
+            values.put(ProductEntry.COLUMN_NAME_UNIT_PRICE, product.getUnitprice());
+
+            if (c.getCount() > 0) {
+                c.moveToFirst();
+                String name = c.getString(c.getColumnIndex(
+                        ProductEntry.COLUMN_NAME_NAME));
+
+                float unitPrice = Float.valueOf(c.getString(c.getColumnIndex(
+                        ProductEntry.COLUMN_NAME_UNIT_PRICE)));
+
+                boolean active = c.getString(c.getColumnIndex(
+                        ProductEntry.COLUMN_NAME_ACTIVE)).equals("1");
+
+                if (!name.equals(product.getName()) || unitPrice != product.getUnitprice()
+                        || active != product.getActive()) {
+                    //UPDATE
+                    String where = ProductEntry.COLUMN_NAME_ID + "=" + product.getId();
+                    db.update(ProductEntry.TABLE_NAME, values, where, null);
+                    updatedProducts++;
+                }
+            } else {
+                //INSERT
+                if (db.insert(ProductEntry.TABLE_NAME, null, values) != -1)
+                    newProducts++;
+            }
+            c.close();
+        }
+        db.close();
+        Log.e(TAG, "UPDATED: " + updatedProducts + " and NEW: " + newProducts + " products");
+        if (!updatedAt.isEmpty())
+            saveUpdatedProductDate(context, updatedAt);
+    }
+
+    private static String selectByProductIDQuery(int id) {
+        return "SELECT * FROM " + ProductEntry.TABLE_NAME + " where "
+                + ProductEntry.COLUMN_NAME_ID + "=" + id;
+    }
+
+    public static ArrayList<Product> loadProducts(final Context context) {
+
+        ArrayList<Product> products = new ArrayList<>();
+
+        if (context == null)
+            return products;
+
+        DbHelper mDbHelper = new DbHelper(context);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        if (db == null)
+            return products;
+
+        Cursor cursor = db.rawQuery(ProductContract.SELECT_ALL_ACTIVE_PRODUCTS, null);
+        // if Cursor is contains results
+        if (cursor != null) {
+            // move cursor to first row
+            if (cursor.moveToFirst()) {
+                do {
+                    int id = cursor.getInt(cursor.getColumnIndex(
+                            ProductEntry.COLUMN_NAME_ID));
+
+                    // Get version from Cursor
+                    String name = cursor.getString(cursor.getColumnIndex(
+                            ProductEntry.COLUMN_NAME_NAME));
+
+                    String unitPrice = cursor.getString(cursor.getColumnIndex(
+                            ProductEntry.COLUMN_NAME_UNIT_PRICE));
+
+                    Product prod = new Product();
+                    prod.setUnitprice(Double.valueOf(unitPrice));
+                    prod.setName(name);
+                    prod.setId(id);
+                    products.add(prod);
+                    // move to next row
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
+        db.close();
+        return products;
+    }
+
+    private static void saveUpdatedProductDate(Context context, String date) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getResources().getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("lastUpdatedProductDate", date);
+        editor.apply();
+    }
+
+    public static String lastUpdatedProductDate(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getResources().getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        return sharedPref.getString("lastUpdatedProductDate", "");
+    }
+
+    public static Product getProductByID(Context context, int id) {
+        if (context == null) {
+            Log.e(TAG, "NULL CONTEXT UPDATING PRODUCTS DB");
+            return null;
+        }
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor c = db.rawQuery(selectByProductIDQuery(id), null);
+
+        if (c != null && c.moveToFirst()) {
+            // Get version from Cursor
+            String name = c.getString(c.getColumnIndex(
+                    ProductEntry.COLUMN_NAME_NAME));
+
+            String unitPrice = c.getString(c.getColumnIndex(
+                    ProductEntry.COLUMN_NAME_UNIT_PRICE));
+
+            Product prod = new Product();
+            prod.setUnitprice(Double.valueOf(unitPrice));
+            prod.setName(name);
+            prod.setId(id);
+            c.close();
+            db.close();
+            return prod;
+        } else {
+            if (c != null)
+                c.close();
+            db.close();
+            return null;
+        }
+
+
+    }
+
+    /* Inner class that defines the table contents */
+    static abstract class ProductEntry implements BaseColumns {
+        static final String TABLE_NAME = "products";
+        static final String COLUMN_NAME_ID = "id";
+        static final String COLUMN_NAME_NAME = "name";
+        static final String COLUMN_NAME_UNIT_PRICE = "unitPrice";
+        static final String COLUMN_NAME_ACTIVE = "active";
+    }
+}
