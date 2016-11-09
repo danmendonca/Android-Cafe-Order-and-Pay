0b08c82eb706143529609e9b1b88235b0627615e
Daniel Mendonça <asmendoncadaniel@gmail.com>
2016-11-09 21:18:01 +0000

    keys persistence - generated signatures

--- acme_remote/app.js
+++ acme_remote/app.js
@@ -2,6 +2,46 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var crypto = require('crypto');
+var fs = require('fs');
+
+fs.readFile(process.argv[1] + '/../keys/privatekey', function (err, readPrK) {
+  if (err) {  
+    generateKeys();
+  }
+  else {
+    console.log("read prK:\n" + readPrK);
+  }
+});
+fs.readFile(process.argv[1] + '/../keys/publickey', function (err, readPuK) {
+  if (!err) {
+    console.log("read puK:\n" + readPuK);
+  }
+});
+
+function generateKeys() {
+  var prime_length = 1024;
+  var diffHell = crypto.createDiffieHellman(prime_length);
+  diffHell.generateKeys('hex');
+  var prK = diffHell.getPrivateKey('hex');
+  var puK = diffHell.getPublicKey('hex');
+
+  fs.writeFile(process.argv[1] + '/../keys/privatekey', prK, { flag: 'wx' }, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("wrote prK:\n" + prK);
+  });
+
+  fs.writeFile(process.argv[1] + '/../keys/publickey', puK, { flag: 'wx' }, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("wrote puK:\n" + puK);
+  });
+}
+
+
 module.exports = app; // for testing
 
 var config = {
