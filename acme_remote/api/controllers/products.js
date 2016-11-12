f3571025362665fff210292ee8ca4fc0574fd710
Daniel Mendonça <asmendoncadaniel@gmail.com>
2016-11-12 20:20:53 +0000

    Routes & backend for edition functionalities

--- acme_remote/api/controllers/products.js
+++ acme_remote/api/controllers/products.js
@@ -3,9 +3,12 @@
 var models = require('../../models');
 var Product = models.product;
 
+var env = process.env.NODE_ENV || "development";
+
 module.exports = {
     getProducts: getProducts,
-    createProduct: createProduct
+    createProduct: createProduct,
+    editProduct: editProduct
 };
 
 
@@ -18,7 +21,7 @@ module.exports = {
  */
 function getProducts(req, res) {
     var lastUpdatedAt = req.swagger.params.lastUpdatedAt.value;
-    if(!lastUpdatedAt){
+    if (!lastUpdatedAt) {
         lastUpdatedAt = new Date(1900, 1);
     }
     Product.findAll({
@@ -28,8 +31,8 @@ function getProducts(req, res) {
             }
         }
     }).then(function (values) {
-    	var prods = {};
-    	prods['products'] = values;
+        var prods = {};
+        prods['products'] = values;
         res.json(prods);
     });
 }
@@ -46,16 +49,16 @@ function createProduct(req, res) {
     var productParam = req.swagger.params.productParam.value;
     var uprice = productParam.unitprice
     var name = productParam.name;
-    var active = productParam.active;   
-    if (!name){
+    var active = productParam.active;
+    if (!name) {
         res.status(403);
         throw "invalid product name";
     }
-    if (!active){
+    if (!active) {
         res.status(403);
         throw "active param not set";
     }
-    if (!uprice){
+    if (!uprice) {
         res.status(403);
         throw "invalid unitprice param";
     }
@@ -70,4 +73,57 @@ function createProduct(req, res) {
         res.status(403);
         res.end(JSON.stringify(err));
     });
+}
+
+/**
+ * 
+ * 
+ * @param {any} req
+ * @param {any} res
+ */
+function editProduct(req, res) {
+    var productParam = req.swagger.params.product.value;
+    var uprice = productParam.unitprice;
+    var name = productParam.name;
+    var active = productParam.active;
+    var id = req.swagger.params.id.value;
+
+    Product.findOne({
+        where: {
+            id: id
+        }
+    }).then((p) => {
+        if (!p) {
+            var ErrorResponse = {};
+            ErrorResponse.message = "Resource Not Found";
+            sendResponse(res, ErrorResponse, 404);
+            return;
+
+        }
+        p.update({
+            name: name,
+            active: active,
+            unitprice: uprice
+        }).then((updated) => {
+            sendResponse(res, updated, 200);
+        });
+    })
+}
+
+
+
+/**
+ * 
+ * @param {any} res
+ * @param {any} varToJson
+ * @param {any} responseCode
+ */
+function sendResponse(res, varToJson, responseCode) {
+
+    if (env == "development") {
+        var stringified = JSON.stringify(varToJson);
+        console.log("\nResponse code: " + responseCode + "\nJson: \n" + stringified + "\n");
+    }
+    res.statusCode = responseCode;
+    res.json(varToJson);
 }
